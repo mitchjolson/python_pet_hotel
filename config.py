0aca7243fc0b707c6a51b1dd9436434d327970ea
Mitch Olson <mitchjolson@gmail.com>
2019-08-21 14:10:07 -0500

    init commit

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'tesoHEt2DewUGyj6NOJ7vmsL8bBisdJ0'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/pet_hotel'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
